60f068d7b09304bef7c6c0fc06b4096fe98d4861
smilingjb12 <smilingjb12@gmail.com>
2025-09-05 01:30:56 +0200

    refactor

--- src/server/exchange-rate.ts
+++ src/server/exchange-rate.ts
@@ -1,9 +1,11 @@
-import { createServerFn } from '@tanstack/react-start'
-import { ExchangeRateService } from '~/services/exchange-rate-service'
-import { authMiddleware } from '~/middleware/auth'
+import { createServerFn } from "@tanstack/react-start";
+import { ExchangeRateService } from "~/services/exchange-rate-service";
+import { authMiddleware } from "~/middleware/auth";
 
 export const getExchangeRate = createServerFn({
-  method: 'GET',
-}).middleware([authMiddleware]).handler(async () => {
-  return await ExchangeRateService.getExchangeRate()
+  method: "GET",
 })
+  .middleware([authMiddleware])
+  .handler(async () => {
+    return await ExchangeRateService.getExchangeRate();
+  });
